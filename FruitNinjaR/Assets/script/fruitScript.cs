380644a8dc7125715bf587a27a7dc6b7e8cd2942
evakimox <zhangjn1121@gmail.com>
2019-02-24 13:58:19 -0500

    bouncing fruit, rusty knife

---
+++ FruitNinjaR/Assets/script/fruitScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fruitScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(3, 3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
---
+++ FruitNinjaR/Assets/script/knifeScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class knifeScript : MonoBehaviour {
+
+    public Sprite rustyKnife;
+    bool rusty;
+
+	// Use this for initialization
+	void Start () {
+        rusty = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other){
+        if (other.gameObject.tag == "fruit") {
+            rusty = true;
+            GetComponent<SpriteRenderer>().sprite = rustyKnife;
+        }
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = (pos);
+    }
+}
