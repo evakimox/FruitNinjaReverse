bfa2edfe082b8142393823f6506127ac3112d1a4
evakimox <zhangjn1121@gmail.com>
2019-02-24 18:16:10 -0500

    what I did I forgot. . .

--- FruitNinjaR/Assets/script/fruitScript.cs
+++ FruitNinjaR/Assets/script/fruitScript.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class fruitScript : MonoBehaviour {
 
+    public Vector2 dir;
+
 	// Use this for initialization
 	void Start () {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(3, 3);
+        //if (dir != null)
+        //{
+        //    GetComponent<Rigidbody2D>().velocity = dir * 3;
+        //}
+        //else
+        //{
+            GetComponent<Rigidbody2D>().velocity = new Vector2(3, 3);
+           //Debug.Log("I am in fruit, dir: " + dir);
+        //}
 	}
 	
 	// Update is called once per frame
---
+++ FruitNinjaR/Assets/script/shooterScript.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooterScript : MonoBehaviour {
+
+    public GameObject fruit;
+    public Sprite fruit1;
+    public Sprite fruit2;
+    public Sprite fruit3;
+    public Sprite fruit4;
+    public Sprite fruit5;
+    public Sprite fruit6;
+    public Sprite fruit7;
+    public Sprite emptyFruit;
+    private Sprite[] fruitTypes = new Sprite[8];
+    public GameObject aimIndicator;
+    private bool isAiming;
+    private float targetAngle;
+    private float curAngle;
+    //0: waiting    1: aiming   
+
+	// Use this for initialization
+	void Start () 
+    {
+        Debug.Log("a");
+        fruitTypes[0] = fruit1;
+        fruitTypes[1] = fruit2;
+        fruitTypes[2] = fruit3;
+        fruitTypes[3] = fruit4;
+        fruitTypes[4] = fruit5;
+        fruitTypes[5] = fruit6;
+        fruitTypes[6] = fruit7;
+        fruitTypes[7] = emptyFruit;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isAiming) {
+            Vector3 centerPosition = transform.position;
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            float mousex = mousePosition.x;
+            float mousey = mousePosition.y;
+            float centerx = centerPosition.x;
+            float centery = centerPosition.y;
+            float diffx = mousex - centerx;
+            float diffy = mousey - centery;
+            float dist = Mathf.Sqrt(Mathf.Pow(diffx, 2) + Mathf.Pow(diffy, 2));
+            float mouseAngle = Mathf.Acos(diffx / dist);
+            Debug.Log(mouseAngle/3.14 * 180);
+            targetAngle = mouseAngle - (Mathf.PI) / 2;
+        }
+        Debug.Log("current angle: " + transform.rotation.eulerAngles);
+        if(transform.rotation.eulerAngles.z > 360f) { 
+            
+        }
+        transform.Rotate(0, 0, targetAngle);
+
+    }
+
+    void OnMouseDown() { 
+        isAiming = true;
+        aimIndicator.GetComponent<SpriteRenderer>().sprite = fruitTypes[Random.Range(0, 6)];
+        //Debug.Log("mouse:" + Camera.main.ScreenToWorldPoint(Input.mousePosition));
+    }
+
+    private void OnMouseUp()
+    {
+        if (isAiming) {
+            GameObject createdFruit = Instantiate(fruit, transform.position, Quaternion.identity);
+            createdFruit.GetComponent<SpriteRenderer>().sprite = aimIndicator.GetComponent<SpriteRenderer>().sprite;
+        }
+        isAiming = false;
+        aimIndicator.GetComponent<SpriteRenderer>().sprite = emptyFruit;
+    }
+}
